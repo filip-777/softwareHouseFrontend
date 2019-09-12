f76bfaf17656ad9d606ea6b8624cff4915b79293
Filip Sęk <filipsek@live.com>
2019-09-12 16:14:44 +0200

    ADD: car add component

--- src/app/components/car/car-form/car-form.component.ts
+++ src/app/components/car/car-form/car-form.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CarService} from '../../../services/car/car.service';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Car} from '../../../models/car/car';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-car-form',
@@ -7,9 +11,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarFormComponent implements OnInit {
 
-  constructor() { }
+  noData = 'no data';
+
+  basicForm: FormGroup;
+  car: Car;
+
+  constructor(private carService: CarService,
+              private fb: FormBuilder,
+              private router: Router) {
+
+  }
 
   ngOnInit() {
+    this.basicForm = this.fb.group({
+      mark: [null, Validators.required],
+      model: [null, Validators.required],
+      color: [this.noData],
+      bodyType: [this.noData],
+      yearOfProduction: [null, Validators.required],
+      price: [null, Validators.required]
+    });
+  }
+
+  save(mark, model, color, bodyTpe, yearOfProduction, price) {
+    this.car = new Car(model, mark, color, bodyTpe, price, yearOfProduction);
+    this.carService.save(this.car).then(response =>
+    this.router.navigate(['/car-list']));
+
   }
 
 }
--- src/app/models/car/car.ts
+++ src/app/models/car/car.ts
@@ -6,4 +6,15 @@ export class Car {
   bodyType: string;
   price: number;
   yearOfProduction: number;
+
+  constructor(model: string, mark: string, color: string, bodyType: string, price: number, yearOfProduction: number) {
+    this.model = model;
+    this.mark = mark;
+    this.color = color;
+    this.bodyType = bodyType;
+    this.price = price;
+    this.yearOfProduction = yearOfProduction;
+  }
+
+
 }
